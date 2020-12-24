b894f3f4daf41fbc286246f68ec288185386d632
AB Noori <ab@ABs-MacBook-Pro.local>
2020-12-23 23:31:10 -0800

    set urls paths to views/corrected error

--- store/urls.py
+++ store/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path 
+from django.urls import path
+
 from . import views
 
-urlspatterns = [
-    #Empty string for base url 
-    path('', views.store, name="store"),
-    path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
+urlpatterns = [
+        #Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
 
 ]
--- store/views.py
+++ store/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 def store(request):
-    context {}
+    context = {}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context {}
+    context = {}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context {}
+    context = {}
     return render(request, 'store/checkout.html', context)
 
